5d9aa06356e71b66c519c1a7bacf595fd8bb9566
Khudyakov1 <notkhudyakov@gmail.com>
2022-11-04 13:50:17 +0300

    Added autostart scripts

---
+++ .config/startup/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Commands to be able to properly use the touchpad
+
+xinput set-prop 18 "libinput Tapping Enabled" 1
+xinput set-prop 18 312 1 
+synclient TapButton1=1
+synclient HorizTwoFingerScroll=1
+synclient VertScrollDelta=-95
+synclient HorizScrollDelta=-95
+synclient TapButton2=3
+
+# Configure the hotkey to change keyboard layout
+setxkbmap us,ru -option 'grp:alt_shift_toggle'
